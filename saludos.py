caff1d0cf8b1b067b694d9619477c04c928aceac
Joseoliva762Win <joseolivacr@unimagdalena.edu.co>
2019-07-20 17:11:11 -0500

    Conversor de divisas

--- saludos.py
+++ saludos.py
@@ -1,2 +1,8 @@
 name = str(input('¿Cual es tu Nombre?:  '))
 print('Hola, ' + name + '!')
+print('Ejemplo de Funcion.')
+
+def suma(dato1, dato2):
+    return dato1 + dato2
+
+print('Suma: '+ str(suma(2,3)))
