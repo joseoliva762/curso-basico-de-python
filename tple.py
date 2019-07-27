bc4084c8d2e4e6360db109f13641c49d112a77f9
Joseoliva762Win <joseolivacr@unimagdalena.edu.co>
2019-07-26 20:26:38 -0500

    Ejemplo de encontrar la primera letra que no se repite con tuplas

--- tple.py
+++ tple.py
@@ -4,55 +4,31 @@
 "abcdefghijklmnopqrstuvwxyziflskecznslkjfabe" d
 "bcccccccccccccyb" y
 """
-def exist(repet_in_text, letter_in_text):
-    nonExist = True
-    for cont in range(len(repet_in_text)):
-        if repet_in_text[cont] == 1:
-            nonExist = False
-            break
-        else:
-            continue
-    if nonExist == False:
-        return letter_in_text[cont]
-    else:
-        return '_'
 
 def first_not_repeating_char(char_sequence):
-    if char_sequence == 'exit':
-        return 'exit'
-    letter_in_text = list()
-    repet_in_text = list()
-    for letter in char_sequence:
-        if letter != ' ':
-            repet = 0
-            try:
-                letter_in_text.index(letter)
-            except ValueError:
-                letter_in_text.append(letter)
-                for verifyLetter in char_sequence:
-                    if verifyLetter == letter:
-                        repet += 1
-                repet_in_text.append(repet)
-    ##print(letter_in_text)
-    ##print(repet_in_text)
-    return exist(repet_in_text, letter_in_text)
+    seen_letter = dict()
+    for idx, letter in enumerate(char_sequence):
+        if letter not in seen_letter:
+            seen_letter[letter] = (idx, 1)
+        else:
+            seen_letter[letter] = (seen_letter[letter][0], seen_letter[letter][1] + 1)
+    final_letter = []
+    for key, value in seen_letter.items():
+            if value[1] == 1:
+                final_letter.append((key,value[0]))
+    not_repited_letter = sorted(final_letter, key=lambda value: value[1])
+
+    if not_repited_letter:
+        return not_repited_letter[0][0]
+    return '_'
 
 
-def main():
+if __name__ == '__main__':
     char_sequence = str(input('Escribe una secuencia de caracteres: '))
 
     result = first_not_repeating_char(char_sequence)
 
     if result == '_':
         print('Todos los caracteres se repiten.')
-    elif result == 'exit':
-        return 'exit'
     else:
         print('El primer caracter no repetido es: {}'.format(result))
-
-if __name__ == '__main__':
-    while True:
-        exit = main()
-        if exit == 'exit':
-            break
-        else: continue
